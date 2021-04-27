3bce33de41c3bb66e0cc8fadd9aaa7ab8d890ff0
mr-atta <mohammadatta97@gmail.com>
2021-04-27 15:36:57 +0300

    lal03

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # About-Me
+*  I am *** Mohammad Atta *** , and this repository is about me 
+   * you can know more information about me here 
+
--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-'use strict';
+'use strict' ;
 
 let username =  prompt('please enter your name ?'); 
 console.log(username);
 
-alert('hello ' + username );
+ alert('hello ' + username );
 
 let ans1 =  prompt('Do you know how to swim ?').toLowerCase(); 
 
@@ -39,3 +39,42 @@ function condition (ans){
 }
 
 alert('Thanks for your time , See you later.');
+
+let graed = 0 ;
+let num = 8 ;
+
+for(let i = 1; i <= 4 ;i++) {
+ let ans6 =  prompt(' guess a number.'); 
+ console.log("The Entered Number Is " + ans6 );
+ if(ans6 == num){ 
+   graed++ ;
+   break ;
+   }
+ if(ans6 > num){ alert('TOO high'); }
+ if(ans6 < num){ alert('TOO low'); }
+}
+alert('The Current  Number is ' + num );
+alert('The Graed ' + graed);
+
+alert('WELCOME LEVEL 02 ');
+
+let  Canseres = [5 , 15 , 25 , 50] ;
+
+for(let m = 0 ; m < Canseres.length ; m++){
+  let ans7 =  prompt(' guess a number.'); 
+  console.log("The Entered Number Is " + ans7 );
+ for(let k = 0 ; k < Canseres.length ; k++){
+   
+   if( ans7 == Canseres[k] ){ 
+    graed++ ;
+    break ;
+    }
+  }
+}
+
+console.log("The Graed Is " + graed );
+
+alert('The Current  Number is ' + Canseres);
+alert('The Total Graed ' + graed + '/5');
+
+
